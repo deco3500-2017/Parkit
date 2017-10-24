88c5708c704ceb5e2d4d41835b8087cff49d4739
Jarrod Stone <jarrod.stone1@gmail.com>
2017-10-24 21:01:52 +1000

    Added account details + adding new payment method

--- www/rent.php
+++ www/rent.php
@@ -327,7 +327,7 @@
         <!-- Adding a Credit Card -->
         <div class="row" id="add-credit-card">
             <div class="col-xs-10 col-xs-offset-1 sub-menu-container">
-                <form id="add-car">
+                <form id="add-card">
                         <div class="form-group">
                             <label class="sr-only" for="card-number">Card Number:</label>
                             <input type="text" class="form-control" id="card-number" placeholder="Card Number:">
@@ -356,7 +356,45 @@
         <!-- Payment Details -->
         <div class="row" id="payment-details">
             <div class="col-xs-10 col-xs-offset-1 sub-menu-container">
-                <p>You have no details still</p>
+                <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Account Number</th>
+                                <th>BSB</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>John Farnham</td>
+                                <td>124-001</td>
+                                <td>918560420</td>                    
+                            </tr>
+                        </tbody>
+                    </tbody>
+                </table>
+                <button onclick="addPayment()" type="button" class="btn btn-default text-center center-block">Add Card</button>
+            </div>
+        </div>
+
+        <!-- Adding new Payment Details -->
+        <div class="row" id="add-debit-card">
+            <div class="col-xs-10 col-xs-offset-1 sub-menu-container">
+                <form id="add-payment">
+                        <div class="form-group">
+                            <label class="sr-only" for="payment-name">Full Name:</label>
+                            <input type="text" class="form-control" id="payment-name" placeholder="Full Name:">
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="payment-account">Account Number:</label>
+                            <input type="text" class="form-control" id="payment-account" placeholder="Account Number:">
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="payment-bsb">BSB:</label>
+                            <input type="text" class="form-control" id="payment-bsb" placeholder="BSB:">
+                        </div>
+                        <button onclick="loadPayment()" type="button" class="btn btn-default text-center center-block">Submit</button>
+                </form>
             </div>
         </div>
 
